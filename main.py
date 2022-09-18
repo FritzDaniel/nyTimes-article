d00eb0c4bc89b38b9e55eb7abfd5266b94891500
fritz-sinaga <danielsinaga53@gmail.com>
2022-09-19 01:20:25 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import urllib
+from urllib.request import Request
+
+import requests
+import templates
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+apiKey = "JLBtTAFcRJRKAnMSnQ4Ds653FphWu2on"
+
+@app.route("/")
+def home():
+    req = requests.get('https://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/7.json?api-key=' + apiKey)
+    if not req.content:
+        return None
+    return render_template('articles.html', data=req.json())
+
+
+@app.route("/articleDetail/<article_index>")
+def articleDetail(article_index):
+    req = requests.get('https://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/7.json?api-key=' + apiKey)
+    if not req.content:
+        return None
+    json = req.json()
+    if len(json['results'][int(article_index)-1]['media']) > 0:
+        data = {
+            "title": json['results'][int(article_index) - 1]['title'],
+            "published_date": json['results'][int(article_index) - 1]['published_date'],
+            "byline": json['results'][int(article_index) - 1]['byline'],
+            "abstract": json['results'][int(article_index) - 1]['abstract'],
+            "img_url": json['results'][int(article_index) - 1]['media'][0]['media-metadata'][2]['url'],
+            "url": json['results'][int(article_index) - 1]['url']
+        }
+    elif len(json['results'][int(article_index)-1]['media']) == 0:
+        data = {
+            "title": json['results'][int(article_index) - 1]['title'],
+            "published_date": json['results'][int(article_index) - 1]['published_date'],
+            "byline": json['results'][int(article_index) - 1]['byline'],
+            "abstract": json['results'][int(article_index) - 1]['abstract'],
+            "img_url": "",
+            "url": json['results'][int(article_index) - 1]['url']
+        }
+    return render_template('detail.html', data=data)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
